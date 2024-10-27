3e07b4c39868fe8d26ebd462a293a4003543e5a3
devapp <dev.elmaya07@gmail.com>
2024-10-27 22:08:46 +0700

    update

--- src/app/api/penemu/route.js
+++ src/app/api/penemu/route.js
@@ -13,3 +13,17 @@ export async function GET() {
 
   return NextResponse.json(data);
 }
+
+export async function POST(request) {
+  const { title, body, author } = await request.json();
+  console.log(title)
+  const { data, error } = await supabase
+    .from('penemu')
+    .insert([{ title, body, author }]);
+
+  if (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+
+  return NextResponse.json(data, { status: 201 });
+}
